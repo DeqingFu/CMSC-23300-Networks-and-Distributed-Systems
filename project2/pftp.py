23f7c01fd1959bf6125486911fd3fc7c9ce2b49f
DeqingFu <deqing@uchicago.edu>
2018-11-21 14:24:04 -0600

    ehhh

--- project2/pftp.py
+++ project2/pftp.py
@@ -59,7 +59,10 @@ def single_threaded_main(args):
     elif code == 200:  
       send_and_log(s, args, log, "PASV\n")
     elif code == 227: # entering passive mode -> get port number
-      return_info = list(eval(recv.split()[-1][:-1]))
+      try:
+        return_info = list(eval(recv.split()[-1][:-1]))
+      except:
+        return_info = list(eval(recv.split()[-1]))
       portno = return_info[-1] + return_info[-2] * 256
       break
     ## error messages ##
@@ -155,7 +158,10 @@ def multi_threaded_worker(args, config_lines, thread_id, block_size, log_file, n
     elif code == 200: 
       send_and_log(s, args, log, "PASV\n", thread_id)
     elif code == 227: # entering passive mode
-      return_info = list(eval(recv.split()[-1][:-1]))
+      try:
+        return_info = list(eval(recv.split()[-1][:-1]))
+      except:
+        return_info = list(eval(recv.split()[-1][:-1]))
       portno = return_info[-1] + return_info[-2] * 256
       send_and_log(s, args, log, ("REST " + str(offset) + "\n"), thread_id)
     elif code == 350:
