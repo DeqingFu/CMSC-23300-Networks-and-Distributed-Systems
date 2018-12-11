c5af1d1799ccf9e750f6d51ad7cf476ceed36414
DeqingFu <deqing@uchicago.edu>
2018-12-11 17:14:42 -0600

    wtf

--- project3/mcrawl.cpp
+++ project3/mcrawl.cpp
@@ -14,6 +14,7 @@
 #include <mutex>
 #include <arpa/inet.h>
 #include <netdb.h>
+#include <stdio.h>
 
 using namespace std;
 
@@ -27,6 +28,52 @@ struct sockaddr_in serv_addr;
 queue<string> q; //downloading queue
 set<string> visited; //visited array
 
+
+
+void crawl_html(string html, int sockfd) {
+    char sending[128];
+    char receiving[1024];
+    snprintf(sending, sizeof(sending), "GET /%s HTTP/1.1\r\nHost: %s\r\n\r\n" , html.c_str() ,hostname.c_str());
+    int n = sendto(sockfd, sending, strlen(sending), 0, (struct sockaddr *)&serv_addr, sizeof(serv_addr));
+    //FILE *fd = fopen("tmp", "rw");
+    while (1) {
+        n = recv(sockfd, receiving, sizeof(receiving), 0);
+        if (n == 0) {
+            break;
+        } else {
+            //printf(receiving);
+            cout << receiving;
+            //fprintf(fd, "%s", receiving);
+        }
+        memset(receiving, 0, sizeof(receiving));
+    }
+    //fclose(fd);
+    
+    
+
+    
+    
+
+}
+
+void crawl() {
+    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (connect(sockfd,(struct sockaddr *)&serv_addr,sizeof(serv_addr)) < 0)  {
+        cout << "error" << endl;
+        exit(1); 
+    }
+    string html; 
+    while (1) {
+        if (q.empty()) {
+            break;
+        } else {
+            html = q.front();
+            cout << html << endl;
+            q.pop();
+            crawl_html(html, sockfd);
+        }
+    }
+}
 
 int main(int argc, char **argv) {
     int opt;
@@ -68,7 +115,7 @@ int main(int argc, char **argv) {
     bzero((char*) &serv_addr, sizeof(serv_addr));
     serv_addr.sin_family = AF_INET;
     bcopy((char*)server->h_addr, (char*)&serv_addr.sin_addr.s_addr, server->h_length);
-    serv_addr.sin_port = port;
+    serv_addr.sin_port = htons(port);
 
     // creating file
     errno = 0;
@@ -80,8 +127,7 @@ int main(int argc, char **argv) {
 
     // Initializing downloading queue
     q.push("index.html");
-
-
+    crawl();
     return 0;
 }
 
